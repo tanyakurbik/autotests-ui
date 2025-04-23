405b340d266d8c36702d718083dbc5e27aa3e92e
tanyakurbik <tanyakurbatova1@gmail.com>
2025-04-23 14:08:48 +0200

    Знакомство с PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -27,34 +27,36 @@ def test_empty_courses_list(chromium_page_with_state: Page):
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_create_course(
-    courses_list_page: CoursesListPage,
-    create_course_page: CreateCoursePage
-):
+def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
     create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+
     create_course_page.check_visible_create_course_title()
     create_course_page.check_disabled_create_course_button()
     create_course_page.check_visible_image_preview_empty_view()
-    create_course_page.check_visible_image_upload_view()
+    create_course_page.check_visible_image_upload_view(is_image_uploaded=False)
     create_course_page.check_visible_create_course_form(
         title="",
-        description="",
-        estimated_time="",
         max_score="0",
-        min_score="0")
+        min_score="0",
+        description="",
+        estimated_time=""
+    )
+
     create_course_page.check_visible_exercises_title()
     create_course_page.check_visible_create_exercise_button()
     create_course_page.check_visible_exercises_empty_view()
-    create_course_page.upload_preview_image(file="./testdata/files/image.png")
+
+    create_course_page.upload_preview_image("./testdata/files/image.png")
     create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
     create_course_page.fill_create_course_form(
         title="Playwright",
-        estimated_time="2 weeks",
-        description="Playwright",
         max_score="100",
-        min_score="10"
+        min_score="10",
+        description="Playwright",
+        estimated_time="2 weeks"
     )
     create_course_page.click_create_course_button()
+
     courses_list_page.check_visible_courses_title()
     courses_list_page.check_visible_create_course_button()
     courses_list_page.check_visible_course_card(
