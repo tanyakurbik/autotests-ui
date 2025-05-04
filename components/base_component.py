119b588e9d4b0ad0c0c56a1f36a655df8f1ac069
tanyakurbik <tanyakurbatova1@gmail.com>
2025-05-04 23:52:02 +0200

    Логирование взаимодействий со страницей. Практика

--- components/base_component.py
+++ components/base_component.py
@@ -1,5 +1,8 @@
 from typing import Pattern
 from playwright.sync_api import Page, expect
+from tools.logger import get_logger
+
+logger = get_logger("BASE_COMPONENT")
 
 
 class BaseComponent:
@@ -7,4 +10,7 @@ class BaseComponent:
         self.page = page
 
     def check_current_url(self, expected_url: Pattern[str]):
+        step = f'Checking that current url matches pattern "{expected_url.pattern}"'
+
+        logger.info(step)
         expect(self.page).to_have_url(expected_url)
--- elements/button.py
+++ elements/button.py
@@ -1,6 +1,9 @@
 from playwright.sync_api import expect
 import  allure
 from elements.base_element import BaseElement
+from tools.logger import get_logger
+
+logger = get_logger("BUTTON")
 
 
 class Button(BaseElement):
@@ -9,11 +12,15 @@ class Button(BaseElement):
         return "button"
 
     def check_enabled(self, nth: int = 0, **kwargs):
-        with allure.step(f'Checking that {self.type_of} "{self.name}" is enabled'):
+        step = f'Checking that {self.type_of} "{self.name}" is enabled'
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             expect(locator).to_be_disabled()
 
     def check_disabled(self, nth: int = 0, **kwargs):
-        with allure.step(f'Checking that {self.type_of} "{self.name}" is disabled'):
+        step = f'Checking that {self.type_of} "{self.name}" is disabled'
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             expect(locator).to_be_disabled()
